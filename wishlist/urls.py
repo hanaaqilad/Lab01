91013bcfea8946fec7772c672b231da2cd17f85c
Hana <hana@Hanas-MacBook-Pro.local>
2022-09-22 08:10:00 +0700

    push lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,6 +3,10 @@ from wishlist.views import show_wishlist
 from wishlist.views import show_json_by_id, show_xml
 from wishlist.views import show_json, show_xml_by_id
 
+from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
+
 app_name = 'wishlist'
 
 urlpatterns = [
@@ -10,5 +14,8 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
-    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id')
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
